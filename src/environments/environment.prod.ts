d83efc46ff5cfa2c6d3e1f36e5a8850f7b9ed122
AlbertoAdame <alberto.aladamero@gmail.com>
2023-06-16 20:44:14 +0200

    acabado v2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,14 @@
 export const environment = {
   production: true,
-  url: 'https://adamroldanapi-production.up.railway.app/'
-  // url: 'http://localhost:8086/'
+  url: 'https://adamroldanapi-production.up.railway.app/',
+  // url: 'http://localhost:8086/',
+  firebaseConfig: {
+    apiKey: "AIzaSyD0VIF4tWVb8MSLY1T8nQZWU9c88BOYT3U",
+    authDomain: "adamroldan-web.firebaseapp.com",
+    projectId: "adamroldan-web",
+    storageBucket: "adamroldan-web.appspot.com",
+    messagingSenderId: "137860762844",
+    appId: "1:137860762844:web:30f112c4c571b4b8aaf819",
+    measurementId: "G-PQYN7MLLJS"
+  }
 };
